6cb25f5a0fb5ea25ed9916262cd8d631ea1bce8e
Pavan Pisupati <mrunalinidivya@gmail.com>
2019-02-28 13:42:50 -0500

    SetUp Heorku

--- server/server.js
+++ server/server.js
@@ -4,8 +4,10 @@ var bodyParser= require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/Todo');
 var {User} = require('./models/User');
-
+const {ObjectID} = require('mongodb');
 var app = express();
+
+const port = process.env.PORT||3000;
 
 app.use(bodyParser.json());
 
@@ -29,9 +31,24 @@ app.get('/todos',(req,res)=>{
   })
 });
 
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
-app.listen((3000),()=>{
-  console.log('Started on port 3000');
+  Todo.findById(id).then((todo)=>{
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((err)=>{
+    returnres.status(400).send();
+  });
+
+})
+app.listen(port,()=>{
+  console.log(`Started on port ${port}`);
 });
 
 module.exports={app};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,13 @@
 const expect = require('expect');
 const request = require('supertest');
-
+const {ObjectID} = require('mongodb')
 const {app} = require('./../server');
 var {Todo} = require('./../models/Todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
-},{text:'Second test todo'}];
+},{_id:new ObjectID(),text:'Second test todo'}];
 
 //this is just for post
 // beforeEach((done) => {
@@ -72,4 +73,31 @@ describe('GET /todos ',()=>{
     })
     .end(done);
   });
-})
+});
+
+describe('GET /todos/:id',()=>{
+it('Should return todo doc',(done)=>{
+  request(app)
+  .get(`/todos/${todos[0]._id.toHexString()}`)
+  .expect(200)
+  .expect((res)=>{
+    expect(res.body.todo.text).toBe(todos[0].text);
+  })
+  .end(done);
+});
+
+it('Should return 404 if todo not found ',(done)=>{
+request(app)
+.get(`/todos/${todos[0]._id.toHexString()}c`)
+.expect(404)
+.end(done);
+});
+
+it('Should return 404 for non-object ids ',(done)=>{
+request(app)
+.get('/todos/123')
+.expect(404)
+.end(done);
+});
+
+});
