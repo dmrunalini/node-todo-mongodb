b4f7084c97d9d8f8f1464568d398a25411ee4271
Pavan Pisupati <mrunalinidivya@gmail.com>
2019-02-28 17:54:02 -0500

    PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var bodyParser= require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser= require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
+const {mongoose} = require('./db/mongoose');
 var Todo = require('./models/todo').Todo;
 var User = require('./models/user').User;
 const {ObjectID} = require('mongodb');
@@ -52,6 +53,33 @@ Todo.findByIdAndRemove(id).then((todo)=>{
 }).catch((err)=>{
   res.status(400).send();
 });
+});
+
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
 });
 
   Todo.findById(id).then((todo)=>{
