a53773cc050fa7127404ad0d48b1826203130565
Pavan Pisupati <mrunalinidivya@gmail.com>
2019-02-28 13:47:07 -0500

    Todo to todo

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser= require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/Todo');
+var {Todo} = require('./models/todo');
 var {User} = require('./models/User');
 const {ObjectID} = require('mongodb');
 var app = express();
